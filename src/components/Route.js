44cc1ca334b9074f573c94eb59822bf57c5db7f6
AlinaKalen <alina103599@gmail.com>
2024-02-02 16:27:40 +0000

    upd

--- src/components/Route.js
+++ src/components/Route.js
@@ -1,14 +1,3 @@
-// import { Navigate } from 'react-router-dom';
-// import { useAuth } from './UseAut/UseAut';
-
-// const PrivateRoute = ({ component: Component, redirectTo = '/' }) => {
-//   const { isLoggedIn, isRefreshing } = useAuth();
-//   const shouldRedirect = !isLoggedIn && !isRefreshing;
-
-//   return shouldRedirect ? <Navigate to={redirectTo} /> : Component;
-// };
-
-// export default PrivateRoute;
 import { Navigate } from 'react-router-dom';
 import { useAuth } from './UseAut/UseAut';
 
@@ -16,7 +5,25 @@ const PrivateRoute = ({ component: Component, redirectTo = '/' }) => {
   const { isLoggedIn, isRefreshing } = useAuth();
   const shouldRedirect = !isLoggedIn && !isRefreshing;
 
-  return shouldRedirect ? <Navigate to={redirectTo} /> : <Component />;
+  return shouldRedirect ? <Navigate to={redirectTo} /> : Component;
 };
 
 export default PrivateRoute;
+// import React from 'react';
+// import { Route, Navigate } from 'react-router-dom';
+// import { useAuth } from './UseAut/UseAut';
+
+// const PrivateRoute = ({ element: Element, redirectTo = '/' }) => {
+//   const { isLoggedIn, isRefreshing } = useAuth();
+//   const shouldRedirect = !isLoggedIn && !isRefreshing;
+
+//   return (
+//     <Route
+//       element={shouldRedirect ? <Navigate to={redirectTo} /> : <Element />}
+//     />
+//   );
+// };
+
+// export default PrivateRoute;
+
+
